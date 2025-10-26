7091bc20daf7dd91cbbff24afb5230b245e2a383
leandro correia <leandronunescorreia@outlook.com>
2025-10-26 09:57:54 +0000

    Add debug build definitions and enhance tests for DeviceManager; include checks for adapter and device creation limits

--- test/test_device_manager.cpp
+++ test/test_device_manager.cpp
@@ -8,3 +8,36 @@ TEST(DeviceManagerTest, InitializeAndRun) {
 
     EXPECT_NE(instance, nullptr);
 }
+
+
+TEST(DeviceManagerTest, CreateAdapterAndDevice) {
+    DeviceManager deviceManager;
+    WGPUInstance instance = deviceManager.getInstance();
+
+    EXPECT_NE(instance, nullptr);
+
+    WGPUAdapter adapter = deviceManager.createAdapter(instance);
+    EXPECT_NE(adapter, nullptr);
+
+    WGPUDevice device = deviceManager.createDevice(adapter);
+    EXPECT_NE(device, nullptr);
+}
+
+
+TEST(DeviceManagerTest, QueryDeviceLimits) {
+    DeviceManager deviceManager;
+    WGPUInstance instance = deviceManager.getInstance();
+
+    EXPECT_NE(instance, nullptr);
+
+    WGPUAdapter adapter = deviceManager.createAdapter(instance);
+    EXPECT_NE(adapter, nullptr);
+
+    WGPUDevice device = deviceManager.createDevice(adapter);
+    EXPECT_NE(device, nullptr);
+
+    WGPULimits limits = deviceManager.queryDeviceLimits(device);
+    // Basic sanity checks: many limits should be positive/non-zero
+    EXPECT_GT(limits.maxTextureDimension2D, 0u);
+    EXPECT_GT(limits.maxBindGroups, 0u);
+}
